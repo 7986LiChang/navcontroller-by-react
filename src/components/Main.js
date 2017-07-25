6b9cd34e76b782b8f6aa2a272478f9c3757925b0
lichang <798602874@qq.com>
2017-07-25 19:06:08 +0800

    实现navbar,但点击同时active两个item

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,38 @@
-require('normalize.css/normalize.css');
-require('styles/App.scss');
+import 'normalize.css/normalize.css';
+import 'styles/App.scss';
 
 import React from 'react';
 
+let liItemDatas = require('../data/liItemDatas.json');
+
 //nav中的li组件
-export class navItem extends React.Component{
+class NavItem extends React.Component{
 	constructor(props){
 		super(props);
+		this.handleClick = this.handleClick.bind(this);
+
 	}
 
 	static defaultProps = {
 
 	};
 
+	点击某个item
+	handleClick(e){
+		//active该item
+		
+		this.props.itemActive();
+
+		e.stopPropagation();
+		e.preventDefault();
+	}
+
 	render(){
+		let aClassName = (this.props.isactive.isActive) ? 'is-active' : 'is-noactive';
+
 		return (
 			<div>
-				<li></li>
+				<li><a className={aClassName} onClick={this.handleClick}>{this.props.data.content}</a></li>
 			</div>
 		);
 	}
@@ -25,14 +41,35 @@ export class navItem extends React.Component{
 export default class AppComponent extends React.Component {
 	constructor(props){
 		super(props);
+		this.state = {
+			navItemStateArr: [
+				// {
+				// 		isActive: false
+				// }
+			]
+		};
+
 	}
 
 	static defaultProps = {
 
 	};
+	
 
 	render() {
-		var navItems = [];
+		let navItems = [];
+		let navItemStateArr = this.state.navItemStateArr;
+
+		liItemDatas.forEach( function(value, index){
+			//初始化isActive
+			if(!navItemStateArr[index]){
+				navItemStateArr[index] = {
+					isActive: false
+				};
+			}
+
+			navItems.push(<NavItem key={index} data={value} isactive={navItemStateArr[index]} itemActive={this.itemActive(index)}/>);
+		}.bind(this));
 
 	    return (
 	    	<div className="main">
@@ -43,10 +80,22 @@ export default class AppComponent extends React.Component {
 	    		</div>
 	    		
 	    		<div className="main-content">
-
+	    			<p>hihi<br/>hihih</p>
 	    		</div>
 	    	</div>
 	    );
   	}
+
+  	itemActive(index){
+  		return function(){
+			let navItemStateArr = this.state.navItemStateArr;
+
+			navItemStateArr[index].isActive = true;
+  			this.setState({
+  				navItemStateArr: navItemStateArr
+  			});
+  		}.bind(this);
+  	}
+
 }
 
